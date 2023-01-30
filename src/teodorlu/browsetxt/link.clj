ced95475461e9f7dba1e3e8d9786fdf9922340b4
Teodor Heggelund <git@teod.eu>
2023-01-30 19:11:59 +0100

    Handle ./.. link special case

--- src/teodorlu/browsetxt/link.clj
+++ src/teodorlu/browsetxt/link.clj
@@ -17,11 +17,15 @@
     (str/starts-with? link "http://") link
     (str/starts-with? link "https://") link
 
+    ;; .. and ./.. links up
+    (= link "..") (dir (ensure-zero-trailing-slashes root))
+    (= link "./..") (dir (ensure-zero-trailing-slashes root))
+
     ;; in a relative link, replace ./ with the root dir
     (str/starts-with? link "./") (str/replace link #"^\./" (ensure-single-trailing-slash root))
-    (= link "..") (dir (ensure-zero-trailing-slashes root))
 
-    ;; we don't support other links (yet)
+    ;; we don't support other links (yet)!
+    ;; (consider using a library for this)
     :else nil
     ))
 
@@ -29,7 +33,9 @@
   (for [ex '[(resolve "https://teod.eu" "./aphorisms/")
              (resolve "https://teod.eu" "https://teod.eu/aphorisms/")
              (resolve "https://teod.eu/aphorisms/" "..")
-             (resolve "https://teod.eu" "https://play.sindre.me")]]
+             (resolve "https://teod.eu" "https://play.sindre.me")
+             (resolve "https://teod.eu/aphorisms/" "./..")
+             ]]
     [ex (eval ex)])
 
   )
