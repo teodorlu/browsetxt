8fac35eb978f4b938c7f5d3becbff49b6cc33eee
Teodor Heggelund <git@teod.eu>
2023-01-30 17:46:52 +0100

    Walk around

--- src/teodorlu/browsetxt.clj
+++ src/teodorlu/browsetxt.clj
@@ -1,11 +1,80 @@
-(ns teodorlu.browsetxt)
+(ns teodorlu.browsetxt
+  (:require
+   [babashka.process :as process]
+   [clojure.edn :as edn]
+   [clojure.string :as str]))
 
-(def start-loc :smalgangen)
+(defn fzf
+  "Choose a thing with fzf
 
-(def goto {:smalgangen #{:g17 :ibv}
-           :g17 #{:smalgangen}
-           :ibv #{:smalgangen}})
+  (fzf [\"apples\" \"pears\" \"pizza\")
+  ;; => \"apples\"     ; depending on the user's choice!
+
+  returns nil on failure."
+  [choices]
+  (let [fzf-result (process/shell {:out :string
+                                   :in (str/join "\n" choices)}
+                                  "fzf")]
+    (when (= 0 (:exit fzf-result))
+      (str/trim (:out fzf-result)))))
+
+(defn less [s]
+  (process/shell {:in s} "less"))
+
+(defn edn-parse-orelse [s orelse]
+  (try (edn/read-string s)
+       (catch Exception _ orelse)))
+
+(defn fzf-edn [choices default]
+  (let [next (fzf (map pr-str choices))]
+    (edn-parse-orelse next default)))
+
+(defn walk-loop [start goto]
+  (loop [start start]
+    (when-let [next-loc (fzf-edn (goto start) nil)]
+      (recur next-loc))))
+
+(defn walk-show-loop
+  [start show next-loc]
+  (loop [loc start]
+    (when-let [next-loc (fzf-edn (next-loc loc) nil)]
+      (show loc)
+      (recur next-loc))))
+
+(defn walk-show-loop-with-exit
+  [start show next-loc exit?]
+  (loop [loc start]
+    (when-let [next-loc (fzf-edn (next-loc loc) nil)]
+      (when-not (exit? next-loc)
+        (show loc)
+        (recur next-loc)))))
 
 (defn -main [& args]
-  (prn start-loc)
-  (prn goto))
+  (walk-show-loop-with-exit :smalgangen
+                            (comp less pr-str)
+                            (fn next-loc [loc]
+                              (concat [:exit]
+                                      (get
+                                       {:smalgangen #{:g17 :ibv}
+                                        :g17 #{:smalgangen}
+                                        :ibv #{:smalgangen}}
+                                       loc
+                                       #{})))
+                            (fn exit? [loc] (= :exit loc)))
+
+  (comment
+    (walk-show-loop :smalgangen
+                    (comp less pr-str)
+                    {:smalgangen #{:g17 :ibv}
+                     :g17 #{:smalgangen}
+                     :ibv #{:smalgangen}})
+    (do
+      (less (str/join "\n" (range 100)))
+      nil)
+
+    (walk-loop :smalgangen
+               {:smalgangen #{:g17 :ibv}
+                :g17 #{:smalgangen}
+                :ibv #{:smalgangen}}))
+  ;; (prn (fzf-edn [:norway :sweden ["lollololo"]] ::no-match))
+  )
