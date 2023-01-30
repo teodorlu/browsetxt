969d32ccce33db250f14b8ca4c898e74d80fb12d
Teodor Heggelund <git@teod.eu>
2023-01-30 19:33:33 +0100

    Support absolute links

--- src/teodorlu/browsetxt/link.clj
+++ src/teodorlu/browsetxt/link.clj
@@ -11,6 +11,22 @@
 (defn ensure-zero-trailing-slashes [link]
   (str/replace link #"[/]*$" ""))
 
+(defn protocol [link]
+  (let [[_ protocol host path page-stuff] (re-matches #"([a-z]+://)([^/]+)([^#]+)(.*)" link)]
+    protocol))
+
+(defn host [link]
+  (let [[_ protocol host path page-stuff] (re-matches #"([a-z]+://)([^/]+)([^#]+)(.*)" link)]
+    host))
+
+(comment
+  (protocol "https://play.teod.eu/aphorisms/index.html")
+  ;; => "https://"
+
+  (host "https://play.teod.eu/aphorisms/index.html")
+  ;; => "play.teod.eu"
+  )
+
 (defn resolve [root link]
   (cond
     ;; absolute links don't need resolving
@@ -23,6 +39,9 @@
 
     ;; in a relative link, replace ./ with the root dir
     (str/starts-with? link "./") (str/replace link #"^\./" (ensure-single-trailing-slash root))
+
+    ;; absolute links start with /
+    (str/starts-with? link "/") (str (protocol root) (host root) link)
 
     ;; we don't support other links (yet)!
     ;; (consider using a library for this)
